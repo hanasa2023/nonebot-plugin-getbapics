94b515915b8e57f3b162ca395f0f55886ce7547b
hanasa2023 <hanasakayui2022@gmail.com>
2023-10-15 10:45:48 +0800

    none

--- nonebot-plugin-getbapics/config.py
+++ nonebot-plugin-getbapics/config.py
@@ -26,7 +26,7 @@ HELP = """"使用方法: 输入 /setu+[tag]+[x|X|*][num]+量词 使用
                         tag自选， x X *可选，num可选，量词可选 张 个 份
                         已实现空格自由
                     输入 /setu open/close/开启/关闭 r18/ai 开启/关闭 获取r18/ai图功能
-                    输入 /help 获取帮助
+                    输入 /setu 获取 help|帮助 获取帮助
             例子:   /setu
                     /setu 4
                     /setu 美游 x4张
