73b1a4729c08ac1f071a5586e8fd723ef11cad4c
hanasa2023 <hanasakayui2022@gmail.com>
2023-10-15 11:47:34 +0800

    📝:update HELP infomation

--- config.py
+++ config.py
@@ -22,15 +22,16 @@ DATABASE: Path = Path(os.path.dirname(
     os.path.abspath(__file__))).joinpath("./config.db")
 TAG: List[str] = ["BlueArchive | 碧蓝档案 | 蔚蓝档案 | ブルーアーカイブ | ブルアカ"]
 MAX_PICS = 5
-HELP = """"使用方法: 输入 /setu+[tag]+[x|X|*][num]+量词 使用
-                        tag自选， x X *可选，num可选，量词可选 张 个 份
-                        已实现空格自由
-                    输入 /setu open/close/开启/关闭 r18/ai 开启/关闭 获取r18/ai图功能
-                    输入 /setu 获取 help|帮助 获取帮助
-            例子:   /setu
-                    /setu 4
-                    /setu 美游 x4张
-                    /setu x4张
-                    /setu 美游 x4个
-                    /setu 美游 4张
-                    /setu 美游 4"""
+HELP = """
+使用方法: 
+    输入 /setu+[tag]+[x|X|*][num]+量词
+    tag自选， x X *可选，num可选，量词可选 张 个 份
+    输入 /setu open/close/开启/关闭 r18/ai 开启/关闭 获取r18/ai图功能
+    输入 /setu 获取 help|帮助 获取帮助
+例: /setu
+    /setu 4
+    /setu 美游 x4张
+    /setu x4张
+    /setu 美游 x4个
+    /setu 美游 4张
+    /setu 美游 4"""
